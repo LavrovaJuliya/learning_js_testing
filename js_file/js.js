026e3320fad19054ae1fff5cb6d2f79d4e540a36
Julia Lavrova <ulyamosienko@gmail.com>
2021-04-19 18:04:41 +0300

    more_wrapper

--- js_file/js.js
+++ js_file/js.js
@@ -1,4 +1,3 @@
-
 function work(a,b){
    alert(a + b);
 }
@@ -6,10 +5,25 @@ function spy(work){
   return function(){
     return work.apply(this,arguments)
     }
-  }
+}
 work = spy(work);
 work(1,2);
 work(4,5);
+
+function f(x){
+	alert(x);
+}
+function delay(f,ms){
+	return function(){
+		setTimeout(() => f.apply(this,arguments) ,ms);
+	}
+}
+	let f1000 = delay(f,1000);
+	let f1500 = delay(f,1500);
+	f1000("test");
+	f1500("test");
+	
+
 			
 		
 			
