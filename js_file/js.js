0ac2a639c925e7812e3372f0a914f9962fa2cffa
Julia Lavrova <ulyamosienko@gmail.com>
2021-04-21 09:58:03 +0300

    dif_wrap

--- js_file/js.js
+++ js_file/js.js
@@ -1,4 +1,4 @@
-function work(a,b){
+/*function work(a,b){
    alert(a + b);
 }
 function spy(work){
@@ -8,20 +8,32 @@ function spy(work){
 }
 work = spy(work);
 work(1,2);
-work(4,5);
+work(4,5);*/
 
 function f(x){
 	alert(x);
 }
 function delay(f,ms){
 	return function(){
-		setTimeout(() => f.apply(this,arguments) ,ms);
+		console.log(arguments);
+		console.log(ms);
+		let tick =()=>{
+			tick = f.apply(this,arguments);
+			console.log(arguments);
+			console.log(ms);
+			//f(arguments);
+			setTimeout(tick,ms);
+		}
+			//tick = f.apply(this,arguments);
+			setTimeout(tick,ms);
+		
+		
 	}
 }
-	let f1000 = delay(f,1000);
-	let f1500 = delay(f,1500);
-	f1000("test");
-	f1500("test");
+	let f1000 = delay(f,5000);
+	let f1500 = delay(f,10000);
+	f1000("test1");
+	f1500("test2");
 	
 
 			
