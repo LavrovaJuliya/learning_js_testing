aaf53300c8f820663cd7f005160c2caea453ecca
Julia Lavrova <ulyamosienko@gmail.com>
2021-04-23 17:10:12 +0300

    problems_task

--- js_file/js.js
+++ js_file/js.js
@@ -14,26 +14,26 @@ function f(x){
 	alert(x);
 }
 function delay(f,ms){
-	return function(){
+	let iner = function(){
 		console.log(arguments);
-		console.log(ms);
-		let tick =()=>{
-			tick = f.apply(this,arguments);
+		setTimeout(function tick(){
+			console.log("in tick function");
 			console.log(arguments);
 			console.log(ms);
-			//f(arguments);
+			f(arguments[0]);
+			tick.apply(this,arguments);
 			setTimeout(tick,ms);
-		}
-			//tick = f.apply(this,arguments);
-			setTimeout(tick,ms);
-		
-		
+		},ms);
+		console.log(typeof iner);
+		iner.apply(this,arguments);
+		return iner;
 	}
 }
 	let f1000 = delay(f,5000);
-	let f1500 = delay(f,10000);
-	f1000("test1");
-	f1500("test2");
+	console.log(f1000);
+	//let f1500 = delay(f,10000);
+	//f1000("test1");
+	//f1500("test2");
 	
 
 			
