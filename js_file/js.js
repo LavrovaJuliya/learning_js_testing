641c875398a764b9124bf3531296ab5e578f8854
Julia Lavrova <ulyamosienko@gmail.com>
2021-05-10 21:12:45 +0300

    get_set

--- js_file/js.js
+++ js_file/js.js
@@ -10,7 +10,7 @@ work = spy(work);
 work(1,2);
 work(4,5);*/
 
-function f(x){
+/*function f(x){
 	alert(x);
 }
 function delay(f,ms){
@@ -33,12 +33,50 @@ function delay(f,ms){
 	console.log(f1000);
 	//let f1500 = delay(f,10000);
 	//f1000("test1");
-	//f1500("test2");
+	//f1500("test2");*/
+let user1={
+	name:"Ivan Ivanov",
+	bankaccount:100000,
+	get incom (){
+		console.log("user1.get");
+		return 0;
+	},	
+	get allinfo(){
+		return`${this.name},${this.bankaccount},${this.incom}.`;
+	}	
 	
+};
+/*for(let entrie of Object.entries(user1)){
+		console.log(entrie);
+};	*/
 
-			
+console.log(user1.allinfo);
+
+
+
+let user2={
+	name:"Petr Petrov",
+	bankaccount:2000000,
+	get incom (){
+		console.log("user2.get");
+		return this._incom;
 		
+	},	
+	set incom(value){
+		console.log("user2.set");
+		this._incom = value;
 			
-		
+	},
+	get allinfo(){
+		return`${this.name},${this.bankaccount},${this.incom}.`;
+	}	
+};
 	
+	user2.incom = prompt("Укажите ваш ежемесячный доход");
+	console.log(user2.allinfo);
+
 	
+/*for(let entrie of Object.entries(user2)){
+		console.log(entrie);
+};	
+*/
